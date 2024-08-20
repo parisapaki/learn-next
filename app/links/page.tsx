46d231424f336c3744626fe736e0c8c45d2754f7
parisapaki <parisapakii@gmail.com>
2024-08-20 10:20:16 +0330

    add code

--- app/links/page.tsx
+++ app/links/page.tsx
@@ -1,9 +1,9 @@
-export default function Home() {
+export default function Links() {
   return (
     <div className="relative overflow-hidden bg-slate-200">
       <div className="relative flex items-center justify-center min-h-screen py-12 px-4 sm:px-6 lg:px-8">
         <div className="bg-white shadow-xl rounded-lg p-8 max-w-lg w-full text-center">
-          <h1 className="text-4xl text-gray-700 mb-4 font-bold">Home Page</h1>
+          <h1 className="text-4xl text-gray-700 mb-4 font-bold"> Link Page </h1>
         </div>
       </div>
     </div>
